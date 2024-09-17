b5f3ce30e44bfa9b720d4e85afe24eef6cff8c19
Anderson Moreira <andersonmoreira@MacBook-Air-de-Anderson.local>
2024-09-17 10:10:06 -0300

    feat: add route to delete a task

--- index.js
+++ index.js
@@ -32,6 +32,23 @@ app.post("/tasks", async (req, res) => {
   }
 });
 
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const taskId = req.params.id;
+
+    const taskToDelete = await TaskModel.findById(taskId);
+    if (!taskToDelete) {
+      return res.status(500).send("Essa tarefa não foi encontrada.");
+    }
+
+    const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+    res.status(200).send("deletedTask");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });

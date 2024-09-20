36a615c1bfadf9758418d279b400ae05fb007d65
Anderson Moreira <andersonmoreira@MacBook-Air-de-Anderson.local>
2024-09-20 09:23:27 -0300

    feat: move update task endpoint to a controller

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -22,27 +22,7 @@ router.post("/:id", async (req, res) => {
 
 // Rota para atualizar uma tarefa existente por ID
 router.patch("/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-    const taskData = req.body;
-
-    const taskToUpdate = await TaskModel.findById(taskId);
-    const allowedUpdates = ["isCompleted"];
-    const requestedUpdates = Object.keys(taskData);
-
-    for (update of requestedUpdates) {
-      if (!allowedUpdates.includes(update)) {
-        taskToUpdate[update] = taskData[update];
-      } else {
-        return res.status(500).send("Atualização inválida.");
-      }
-    }
-
-    await taskToUpdate.save();
-    return res.status(200).send(taskToUpdate);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).update();
 });
 
 // Rota para deletar uma tarefa por ID
